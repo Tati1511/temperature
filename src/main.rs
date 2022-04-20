5bc4f0dc96a8d53b0c55e6ddedadc98786dc78bb
Tati1511 <102051224+Tati1511@users.noreply.github.com>
2022-04-20 12:52:41 -0400

    if-else

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,66 @@
-fn tell_temperature (temperature:i32)
-{
-
-    if temperature >= 30 {
-        println!("It's hot!");
+fn tell_temperature(temp: i32) {
+    if temp <= 10 {
+        println!("It's cold!");
     } else {
-        println!("It's not hot!");
+        if temp <= 25 {
+            println!("It's nice");
+        } else {
+            if temp <= 30 {
+                println!("It's warm");
+            } else {
+                println!("It's hot!")
+            }
+        }
     }
 }
 
 
+
+fn tell_temperature1(temp: i32) {
+    if temp <= 10 {
+        println!("It's cold!");
+    } else if temp <= 25 {
+        println!("It's nice");
+    } else if temp <= 30 {
+        println!("It's warm");
+    } else {
+        println!("It's hot!")
+    }
+}
+
+fn tell_temperature3 (temp: i32) {
+    let message = if temp <= 10 {
+        "It's cold!"
+    } else if temp <= 25 {
+        "It's nice"
+    } else if temp <= 30 {
+        "It's warm"
+    } else {
+        "It's hot!"
+    };
+
+    println!("{}", message);
+}
+
+fn tell_temperature4(temp: i32) {
+    let message = if temp <= 10 {
+        "It's cold!"
+    } else if temp <= 25 {
+        "It's nice"
+    } else if temp <= 30 {
+        "It's warm"
+    } else {
+        "It's hot!"
+    };
+
+    println!("{}", message);
+}
+
 fn main() {
-    tell_temperature(10);
-    tell_temperature(20);
-    tell_temperature(30);
-    tell_temperature(40);
+    tell_temperature(31);
+    tell_temperature1(15);
+    tell_temperature3(10);
+    tell_temperature4(10);
+
+
 }
